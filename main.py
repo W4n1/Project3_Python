3da40d1aa6d16528aff884b3516b7acfbeca9f80
W4n1 <wanikalimira@hotmail.co.uk>
2021-10-16 23:51:47 +0000

    made the result out come appear in integer format. Input only working on name_of_unit[0] at the moment

--- main.py
+++ main.py
@@ -1,28 +1,40 @@
-months_to_units = "31.5"
-days_to_units = "30"
-durartion_type = [months_to_units, days_to_units]
-name_of_units = ["hours", "minutes", "seconds"]
+
+months_to_units = 31.5
+days_to_units = 30
+durartion_type = (months_to_units, days_to_units)
+name_of_units = ("hours", "minutes", "seconds")
 
 
-def days_to_units_calc(custom_message, number_of_days):
-    print(f"{number_of_days} days are {number_of_days * durartion_type[1]} {name_of_units[0]}")
-    print(f"{number_of_days} days are {number_of_days * durartion_type[1]} {name_of_units[1]}")
-    print(f"{number_of_days} days are {number_of_days * durartion_type[1]} {name_of_units[2]}")
+def days_to_units_calc(number_of_days):
+    return (f"{number_of_days} days are {number_of_days * durartion_type[1]} {name_of_units[0]}")
+    return (f"{number_of_days} days are {number_of_days * durartion_type[1]} {name_of_units[1]}")
+    return (f"{number_of_days} days are {number_of_days * durartion_type[1]} {name_of_units[2]}")
 
 
-    '''print(custom_message)
+'''print(custom_message)
 days_to_units_calc("You have converted days to your preferred unit, please see results below", 35)'''
 
-input("Please enter a number of days you'd like to convert to units\n")
+
+user_day_input = input("Please enter a number of days you'd like me to convert to units!\n")
+user_input_integer = int(user_day_input)
+calc_value = days_to_units_calc(user_input_integer)
+print(calc_value)
 
 
-def months_to_units_calc(customer_message,number_of_days):
-    print(f"{number_of_days} months are {number_of_days * durartion_type[0]} {name_of_units[0]}")
-    print(f"{number_of_days} months are {number_of_days * durartion_type[0]} {name_of_units[1]}")
-    print(f"{number_of_days} months are {number_of_days * durartion_type[0]} {name_of_units[2]}")
+def months_to_units_calc(number_of_days):
+    return (f"{number_of_days} months are {number_of_days * durartion_type[0]} {name_of_units[0]}")
+    return (f"{number_of_days} months are {number_of_days * durartion_type[0]} {name_of_units[1]}")
+    return (f"{number_of_days} months are {number_of_days * durartion_type[0]} {name_of_units[2]}")
 
 
-''' print(customer_message)
+
+'''print(customer_message)
 months_to_units_calc("You have converted months to your preferred unit", 35)'''
 
-input("Please enter a number of months you'd like to convert to units\n")
+user_month_input = input("Please enter a number of months you'd like me to convert to units!\n")
+
+user_input_integer = int(user_month_input)
+calc_value = months_to_units_calc(user_input_integer)
+print(calc_value)  
+
+
