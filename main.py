a54db7c0cb45495627a06572a571daad5b92b683
W4n1 <wanikalimira@hotmail.co.uk>
2021-10-17 05:27:29 +0000

    elif condition added for when user enters 0 to send a message

--- main.py
+++ main.py
@@ -10,16 +10,18 @@ def days_to_units_calc(number_of_days):
         return (f"{number_of_days} days are {number_of_days * durartion_type[1]} {name_of_units[0]}")
         return (f"{number_of_days} days are {number_of_days * durartion_type[1]} {name_of_units[1]}")
         return (f"{number_of_days} days are {number_of_days * durartion_type[1]} {name_of_units[2]}")
+    elif number_of_days == 0:
+        return "You entered 0, please enter a valid positive number to proceed."
     else:
-        return "you have entered a negative value, please enter a positive to proceed."
-    
-    
+        return "You have entered a negative value, please enter a positive to proceed."
+
+
 '''print(custom_message)
 days_to_units_calc("You have converted days to your preferred unit, please see results below", 35)'''
 
-
 user_day_input = input("Please enter a number of days you'd like me to convert to units!\n")
 user_input_integer = int(user_day_input)
+
 calc_value = days_to_units_calc(user_input_integer)
 print(calc_value)
 
@@ -29,8 +31,10 @@ def months_to_units_calc(number_of_days):
         return (f"{number_of_days} months are {number_of_days * durartion_type[0]} {name_of_units[0]}")
         return (f"{number_of_days} months are {number_of_days * durartion_type[0]} {name_of_units[1]}")
         return (f"{number_of_days} months are {number_of_days * durartion_type[0]} {name_of_units[2]}")
+    elif number_of_days == 0:
+        return "You entered 0, please enter a valid positive number to proceed."
     else:
-        return "you have entered a negative value, please enter a positive to proceed."
+        return "You have entered a negative value, please enter a positive to proceed."
 
 
 
@@ -40,7 +44,8 @@ months_to_units_calc("You have converted months to your preferred unit", 35)'''
 user_month_input = input("Please enter a number of months you'd like me to convert to units!\n")
 
 user_input_integer = int(user_month_input)
+
 calc_value = months_to_units_calc(user_input_integer)
-print(calc_value)  
+print(calc_value)
 
 
