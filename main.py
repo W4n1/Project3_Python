33c100adbf394fc45d7c532687967579d0e980b4
W4n1 <wanikalimira@hotmail.co.uk>
2021-10-28 11:00:40 +0000

    created logic for months to unit with if/else statments. started on creating the same thing for the day to unit functions. Also created an intro for useor user

--- main.py
+++ main.py
@@ -1,77 +1,103 @@
-months_to_units = 31  # maybe do it by month rather than a estimate
-days_to_units = 30
-durartion_type = [months_to_units, days_to_units]
-name_of_units = ["hours", "minutes", "seconds"]
+calc_to_hours = 24  # 24hours in a day
+calc_to_minutes = 60  # minutes in an hour
+calc_to_seconds = 3600   # seconds in an hour
+name_unit = "hours", "minutes", "seconds"
 
 
-def days_to_units_calc(number_of_days):
-    return (f"{number_of_days} days are {number_of_days * durartion_type[1]} {name_of_units[0]}")
-    return (f"{number_of_days} days are {number_of_days * durartion_type[1]} {name_of_units[1]}")
-    return (f"{number_of_days} days are {number_of_days * durartion_type[1]} {name_of_units[2]}")
+def months_to_units(unit_chosen) -> object:
+    # hour
+    if unit_chosen == "hours" or unit_chosen == "hour":
+        month_hours()
+        return (f'{user_name}, you have selected months and hours, please enter number of months you would like '
+                f'to be converted:\n')
+    
+    # minute
+    elif unit_chosen == "minutes" or unit_chosen == "minute":
+        # month_minutes ()
+        return (f'{user_name}, you have selected months and minutes, please enter number of months you would like '
+                f'to be converted:\n')
+    
+    # seconds 
+    elif unit_chosen == "seconds" or unit_chosen == "second":
+        # month_seconds()
+        return (f'{user_name}, you have selected months and seconds, please enter number of months you would like '
+                f'to be converted:\n')
+    
 
+def days_to_units(unit_chosen):
+    # hour
+    if unit_chosen == "hours" or unit_chosen == "hour":
+        day_hours()
+        return (f'{user_name}, you have selected months and hours, please enter number of months you would like '
+                f'to be converted:\n')
 
-def months_to_units_calc(number_of_months):
-    if number_of_months > 0:
-        return (f"{number_of_months} months are {number_of_months * durartion_type[0]} {name_of_units[0]}")
-        return (f"{number_of_months} months are {number_of_months * durartion_type[0]} {name_of_units[1]}")
-        return (f"{number_of_months} months are {number_of_months * durartion_type[0]} {name_of_units[2]}")
-
-
-def validate_and_execute_days():
-    try:
-        user_input_integer = int(user_input1)
-        if user_input_integer > 0:
-            calculated_value = days_to_units_calc(user_input_integer)
-            print(calculated_value)
-        elif user_input1 == 0:
-            print("You entered a 0, please enter a valid positive number"
-                  "of days")
-        else:
-            print("You have entered a negative number of days, please enter a"
-                  "valid positive number in order to proceed")
-    except ValueError:
-        print("Your input is not a number of days. To proceed please use a"
-              "number! Do not ruin my program friend!")
-
-
-
-def validate_and_execute_months():
-    try:
-        user_input_integer = int(user_input1)
-        if user_input_integer > 0:
-            calc_value = months_to_units_calc(user_input_integer)
-            print(calc_value)
-        elif user_input1 == 0:
-            print("You entered 0, please enter a valid positive number"
-                  "of months to proceed")
-        else:
-            print("You entered a negative number of months, please enter"
-                  "a valid positive number")
-    except ValueError:
-        print("Your input is not a number of months. To proceed please use"
-              "a number! Do not ruin my program friend!")
-
-
-# looping the logic
-# assign an empty string to variable user_input before the while
-user_input1 = ""
-while user_input1 or user_input != "exit":
-    if user_input1 == "days" or user_input1 == "day":
-        user_input1 = input(f"Please enter a number of day(s) you'd like me to"
-                            "convert to units!\n")
-        validate_and_execute_days()
     else:
-        user_input1 == "months" or user_input1 == "month"
-        user_input1 = input(f"Please enter a number of month(s) you'd like me"
-                            "to convert to units!\n")
-        validate_and_execute_months()
-print('Please enter month(s) or day(s)-\n')
+        return "please enter months or days"
 
 
+def month_hours(num_months):
+    if num_months > 0:
+        return f"{num_months} months are {num_months * calc_to_hours} {name_unit[0]}"
+
+
+
+        '''return (f"{unit_chosen} months are {unit_chosen * calc_to_hours}name_unit[0]")
+
+        # call function for the calculations for months and hours
+
+    elif unit_chosen == 'minutes' or unit_chosen == 'minute':
+        print(f'you have selected months and minutes, please enter number of months you would like to be converted:\n')
+        # call function for the calculations for months and minutes
+
+    elif unit_chosen == 'seconds' or unit_chosen == 'second':
+        print('you have selected months and seconds, please enter number of months you would like to be converted:\n')
+        # call function for the calculations for months and seconds
+
+
+def months_to_units(unit_to_months):
+    # hour
+    if unit_to_months == "hours":
+        return (f"{num_months} months are {num_months * calc_to_hours}name_unit[0]")
+
+        # minute
+    elif unit_to_months == "minutes"
+        print(f"{num_months} months are {num_months * calc_to_minutes}name_unit[1]")
+
+        # second
+        print(f"{num_months} months are {num_months * calc_to_seconds}name_unit[2]")
+    # months_to_units()  #calling the month function
+
+
+# days_to_units()  #calling the day function
+
+
+def days_to_units(num_days):
+    print(f"{num_days} days are { num_days * calc_to_hours}name_unit[0]")
+    print(f"{num_days} days are { num_days * calc_to_minutes}name_unit[1]")
+    print(f"{num_days} days are { num_days * calc_to_seconds}name_unit[2]")
 '''
-> currently it is only converting hours. 
-> there is no real transaction from days to months
-> for the day loop, exit currently means change to months
-> for the month loop, exit finishes the process. 
-> I need to apply the intro in the test section to the this file
-'''
+
+user_name = input("Welcome Friend "
+                  "please enter your name:\n")
+user_input = input(f'Hi {user_name}, WELCOME :) !\n '
+                    'Would you like to convert months or days? '
+                    'Enter months or days\n')
+
+# entering days or months
+if user_input == 'months' or user_input == 'month':
+    print('You have selected months!'
+          'What unit would you like to be converted\n '
+          'hours\n minutes \n seconds?')
+    months_to_units(unit_chosen="hours" or "minutes" or "seconds")
+
+else:
+    user_input == 'days' or user_input == 'day'
+    print('You have selected days!'
+          'What unit would you like to be converted\n '
+          'hours\n minutes \n seconds?')
+    days_to_units(unit_chosen="hours" or "minutes" or "seconds")
+
+
+
+
+# need to create the calculations - accepting user input
--- prac.py
+++ prac.py
@@ -1,12 +1,1 @@
-user_name = input("Welcome user, please enter your name:\n")
-user_input = input(f'Hi {user_name}, WELCOME! Converting months or days to'
-                    'unites, please choose? Choose by typing'
-                    '"months" or "days"\n')
-if user_input == "months" or user_input == "month":
-    from main import validate_and_execute_months
-    validate_and_execute_months()Wan
-elif user_input == "days" or user_input == "day":
-    from main import validate_and_execute_days
-    run_days_function = validate_and_execute_days()
-else:
-    print(f"{user_name}You must enter months or days in order to proceed")
+
